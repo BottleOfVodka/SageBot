6fd9311d4fbd9157d019504f4ed88b5495d8d924
MountainSage <mountainsage@babubox.co>
2016-02-15 09:38:48 +1100

    Create SageBot.py

---
+++ SageBot.py
@@ -0,0 +1,21 @@
+import discord #import discord.py library
+
+client = discord.Client() #Initialise a discord client
+
+#Report if connection was successful and ready for commands
+@client.event
+async def on_ready():
+    print('Connected!')
+    print('Username: ' + client.user.name)
+    print('ID: ' + client.user.id)
+
+#Report if someone starts playing a game
+@client.event
+async def on_member_update(before, after):
+    if str(before.game) != str(after.game) and str(after.game) != 'None':
+      sagegeneral = client.get_channel('channelid')
+      lvmsg = '{} is now playing {}'.format(after.name, after.game)
+      await client.send_message(sagegeneral, lvmsg, tts=False)
+
+if __name__ == '__main__':
+      client.run('email', 'password') #Login using the discord client we initialised
