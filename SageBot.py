d22147fecf5f744d9cd9dbd1d3d3656414c9dc97
MountainSage <mountainsage@babubox.co>
2016-02-19 00:10:03 +1100

    Added - Join Channels, Per Channel Commands

--- SageBot.py
+++ SageBot.py
@@ -1,3 +1,8 @@
+# SAGEBOT DEVELOPMENT BUILD
+# - Ability to Join Servers - Done
+# - Command Enable/Disable Based on Server Topic Tags - Done
+# - Constructed Help Command to Report Availiable Commands
+
 import discord #import discord.py library
 import random #import library for entropy in commands
 import configparser #import library for reading config.ini
@@ -14,7 +19,7 @@ async def on_ready():
 #Report if someone starts playing a game
 @client.event
 async def on_member_update(before, after):
-	if str(before.game) != str(after.game) and str(after.game) != 'None':
+	if str(before.game) != str(after.game) and str(after.game) != 'None' and '[SB]' in before.server.default_channel.topic:
 		lvmsg = '{} is now playing {}'.format(after.name, after.game)
 		await client.send_message(before.server, lvmsg, tts=False)
 
@@ -24,6 +29,9 @@ async def on_message(message):
 	if message.author == client.user:
 		return
 
+	if 'https://discord.gg/' in message.content and message.channel.is_private == True:
+		await client.accept_invite(message.content[message.content.find('https://discord.gg/'):35+message.content.find('https://discord.gg/')])
+
 	if message.content.startswith('.sbi_'):
 		tmpimgtopost = message.content
 		imgtopost = tmpimgtopost[5:]
@@ -31,14 +39,11 @@ async def on_message(message):
 		image = random.choice(open(filetoread).readlines())
 		await client.send_message(message.channel, image, tts=False)
 
-
-
-
 #Run this if the bot is being started not as a master thread
 if __name__ == '__main__':
 	config = configparser.ConfigParser()
 	config._interpolation = configparser.ExtendedInterpolation()
 	config.read('config.ini')
 	config.sections()
-	print('=========\n=SAGEBOT=\n=========')
+	print('=========\n=SAGEBOT=\n=========\nTHIS IS A DEVELOPMENT BUILD')
 	client.run(config.get('DiscordClient', 'email'), config.get('DiscordClient', 'password')) #Login using the discord client we initialised
